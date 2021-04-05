c61b0bb43f9a548ae044b3b86611c8601fb0b698
David <david.mills2@outlook.com>
2021-04-04 23:22:04 -0700

    Add project files.

---
+++ PetPal(MVC-DB-First/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PetPal_MVC_DB_First
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PetPal(MVC-DB-First/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PetPal_MVC_DB_First.MvcApplication" Language="C#" %>
